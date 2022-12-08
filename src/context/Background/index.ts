267db320e7947c03a610f3d296cbd0792dfdcea5
Alexander Roed Thorup <axtru@roedthorup.dk>
2022-12-08 19:54:56 +0100

    Added the version to the package.json, and removed the hot reload accept from the background.

--- src/context/Background/index.ts
+++ src/context/Background/index.ts
@@ -10,14 +10,3 @@ addManagementListeners();
  * Adds the runtime listeners to the background service worker.
  */
 addRuntimeListeners();
-
-// if (module.hot) {
-//   const status: string = module.hot.status();
-
-//   console.log('backgound _> status : ', status);
-
-//   module.hot.accept((error) => {
-//     console.warn('background _> error: ', error);
-//     console.warn('background _> error.cause: ', error.cause);
-//   });
-// }
